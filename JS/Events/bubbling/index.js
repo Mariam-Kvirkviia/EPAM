03732276ce4faca4bdc2f4ca7c5c4bca7e431eca
Mariam Kvirkviia <marikanik1999@gmail.com>
2023-07-29 18:13:38 +0400

    JS storage

--- JS/Events/bubbling/index.js
+++ JS/Events/bubbling/index.js
@@ -2,25 +2,26 @@ let form = document.querySelector("form");
 let div = document.querySelector("div");
 let p = document.querySelector("p");
 
-form.addEventListener("click", function (event) {
-  console.log("form");
+// form.addEventListener("click", function (event) {
+//   console.log(event.bubbles);
+//   console.log("form");
 
-  // console.log('form', event.target.tagName, event.currentTarget.tagName, this.tagName);
-});
+//   // console.log('form', event.target.tagName, event.currentTarget.tagName, this.tagName);
+// });
 
-div.addEventListener("click", function (event) {
-  console.log("div");
-});
-div.addEventListener("click", function (event) {
-  //   event.stopImmediatePropagation();
-  console.log("div2");
-});
-div.addEventListener("click", function (event) {
-  console.log("div3");
-});
+// div.addEventListener("click", function (event) {
+//   console.log("div");
+// });
+// div.addEventListener("click", function (event) {
+//   //   event.stopImmediatePropagation();
+//   console.log("div2");
+// });
+// div.addEventListener("click", function (event) {
+//   console.log("div3");
+// });
 
-p.addEventListener("click", function (event) {
-  // event.stopPropagation();
+// p.addEventListener("click", function (event) {
+//   // event.stopPropagation();
 
-  console.log("p");
-});
+//   console.log("p");
+// });
