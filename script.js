f0d883e9c938774f3ebb14328dd510425e3f7182
Mariam Kvirkviia <marikanik1999@gmail.com>
2023-09-30 20:18:35 +0400

    redux

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(354);
+let a, b, rest;
+[,b] = [10, 20];
+
+
+// Expected output: 10
+
+console.log(b&& 23);
+// Expected output: 20
+
+// [a, b, ...rest] = [10, 20, 30, 40, 50];
+
+// console.log(rest);
+// // Expected output: Array [30, 40, 50]
---
+++ script2.js
@@ -0,0 +1,6 @@
+export function t() {
+  console.log(1);
+}
+export function f() {
+  console.log(0);
+}
